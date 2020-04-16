965572b71116013f1789c5a9d70db7471c30aa0a
Daniel Elias <danielek7@gmail.com>
2020-04-16 21:39:07 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    brand: 'Vue Mastery',
+    product: 'Socks',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link: 'https://google.com',
+    inventory: 0,
+    inStock: false,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart(){
+      this.cart +=1;
+    },
+    updateProduct(variantImage){
+      this.image = variantImage;
+    }
+  },
+  computed: {
+    title(){
+      return this.brand + " " + this.product;
+    }
+  },
+})
